4b3d0258846964a515f420dcc9db342e448f072c
Wouter van den Dries <woutervddries@live.nl>
2023-12-23 16:04:37 +0000

    Add Post model and STATUS choices for draft/published.

--- goodnews/models.py
+++ goodnews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Pusblished"))
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_post")
+    edited_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    # featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
+    reported = models.BooleanField()
+    reports = models.ManyToManyField(Uer, related_name="post_reports")
+
