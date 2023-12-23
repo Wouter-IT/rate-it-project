c3065152a9b09c5bfa76baa7982420792fda7dc5
Wouter van den Dries <woutervddries@live.nl>
2023-12-23 16:19:23 +0000

    Change author to 'user' in post/comment models. Add Meta ordering and __str__ for both modals too.

--- goodnews/models.py
+++ goodnews/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_post")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_post")
     edited_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     # featured_image = CloudinaryField('image', default='placeholder')
@@ -17,15 +17,27 @@ class Post(models.Model):
     likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
     reported = models.BooleanField(default=False)
     reports = models.ManyToManyField(Uer, related_name="post_reports")
+        
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
 
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_comments")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     edited_on = models.DateTimeField(auto_now=True)
     likes = models.ManyToManyField(User, related_name="comment_likes", blank=True)
     reported = models.BooleanField(default=False)
     reports = models.ManyToManyField(Uer, related_name="post_reports")
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
